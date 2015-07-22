328360fca66cd60ffacc56dc1b8593dae8a3db02
TilliWilli <tilliwilli@gmail.com>
2015-07-22 16:02:18 +0300

    2nd commit

--- parserCollections.js
+++ parserCollections.js
@@ -41,8 +41,9 @@ var CParserPartyPoker = CParser.Extend
 	//Methods
 	function Parse(pResource)
 	{
-		var result = {};
+		var _result = {};
 		var games = pResource.split("#Game No :");
+		var gameStats = [];
 		for(var iX=1; iX<games.length; ++iX)
 		{
 			var gameStatements = games[iX].split("\n");
@@ -55,9 +56,47 @@ var CParserPartyPoker = CParser.Extend
 				game.smallBlind = parseFloat( blinds[0].substr(1));
 				game.bigBlind = parseFloat( blinds[1].substr(1));
 			}
-
+			{
+				game.timeStart = gameStatements[2].split("-")[1].trim();
+				game.playersNumber = gameStatements[5].split(":")[1].split("/")[0].trim();
+				game.playersTotal = gameStatements[5].split(":")[1].split("/")[1].trim();
+			}
+			{
+				game.buttonPosition = gameStatements[4].split(" ")[1].trim();
+				for(var iY=0; iY<9; ++iY)
+				{
+					if(gameStatements[6 + iY].indexOf("TilliWilli") !== -1)
+					{
+						game.myPosition = gameStatements[6 + iY].split(":")[0].split(" ")[1].trim();
+						game.depositStart = gameStatements[6 + iY].split("(")[1].split("$")[1].split(" ")[0].trim();
+						break;
+					}
+				}
+			}
+			{
+				var preFlopLineAnchor = 0;
+				var flopLineAnchor = 0;
+				var turnLineAnchor = 0;
+				var riverLineAnchor = 0;
+				for(var iZ=6; iZ<1000; ++iZ)
+				{
+					if(gameStatements[iZ].slice(0, 10) === "** Dealing")
+					{
+						if(gameStatements[iZ].slice(0, 13) === "** Dealing do")
+							preFlopLineAnchor = iZ;
+						if(gameStatements[iZ].slice(0, 13) === "** Dealing Fl")
+							flopLineAnchor = iZ;
+						if(gameStatements[iZ].slice(0, 13) === "** Dealing Tu")
+							turnLineAnchor = iZ;
+						if(gameStatements[iZ].slice(0, 13) === "** Dealing Ri")
+							riverLineAnchor = iZ;
+					}
+				}
+				
+			}
+			gameStats.push(game);
 		}
-		return result = game;
+		return _result = gameStats;
 	}
 );
 CParserPartyPoker.className = "CParserPartyPoker";
